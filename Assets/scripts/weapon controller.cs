897b252e87b68b780dd9df1a0d52fbe972e731e6
Hackssy <hacksyofficial@gmail.com>
2024-02-04 10:06:39 -0800

    Initial commit

---
+++ Assets/scripts/weapon controller.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponcontroller : MonoBehaviour
+{
+    public GameObject bulletprefab;
+    public Transform BulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletprefablifetime = 3f;
+
+    void Update()
+    {
+        //left mouse click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        //make bullet shoot
+        GameObject bullet = Instantiate(bulletprefab, BulletSpawn.position, Quaternion.identity);
+        //shoot bullet
+        bullet.GetComponent<Rigidbody>().AddForce(BulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        //remove da bullet
+        StartCoroutine(DestroybulletAfterTime(bullet, bulletprefablifetime));
+
+
+
+
+    }
+
+    private IEnumerator DestroybulletAfterTime(GameObject bullet, float bulletprefablifetime)
+    {
+        
+    }
+}
